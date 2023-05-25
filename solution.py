0aa39c108d834bf80d94bb8f1d3e4a64122911cc
Matthew Heeter <matthewheeter1611@gmail.com>
2023-05-24 23:15:32 -0400

    Corrected issue with setting null pointers and pointer hopping if deleting last element

--- solution.py
+++ solution.py
@@ -18,8 +18,8 @@ def removeElements(head, value):
         print(current.val)
         if (current.next.val == value):
             current.next = current.next.next
-            current.next.next = None
-        current = current.next
+        if (current.next != None):
+            current = current.next
 
     return new_head
 
@@ -31,14 +31,19 @@ def insert_after(first, second):
         second.next = first.next.next
         first.next = second
 
-A = ListNode(7)
-B = ListNode(3)
-C = ListNode(4)
-D = ListNode(7)
-E = ListNode(9)
+A = ListNode(1)
+B = ListNode(2)
+C = ListNode(6)
+D = ListNode(3)
+E = ListNode(4)
+F = ListNode(5)
+G = ListNode(6)
 insert_after(A,B)
 insert_after(B,C)
 insert_after(C,D)
 insert_after(D,E)
+insert_after(E,F)
+insert_after(F,G)
 
-Z = removeElements(A, 7)
+
+Z = removeElements(A, 6)
